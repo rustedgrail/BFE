9a8f27df1d2d98989a12aeef0b67ffa6fd55bde4
David Newell <beta@alumni.rice.edu>
2011-11-07 11:33:47 -0800

    new skeleton rails app

---
+++ cookbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = 'a264f0b4424da7a6836593e29780ae916141d8114adfc36825df4a15e4911f1ff419a0b9f9b2ffc2f255e643c727dca1f01bb15105bb84e666f0b39de09a3ef6'
